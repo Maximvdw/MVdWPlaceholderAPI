14c18a9a8e788a15a63086bdc4ab79e66c523221
Maximvdw <maxim@mvdw-software.com>
2017-11-20 14:37:33 +0100

    bugfix with triggers

--- src/main/java/be/maximvdw/placeholderapi/internal/eventhooks/BaseEventHook.java
+++ src/main/java/be/maximvdw/placeholderapi/internal/eventhooks/BaseEventHook.java
@@ -8,221 +8,245 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class BaseEventHook implements Listener {
-	private Map<EventCondition, String> eventConditions = new HashMap<EventCondition, String>();
-	private static String actionName = "";
-	/* Plugin information */
-	private String name = "";
-	private String url = "";
-	private String description = "";
-	private String shortName = "";
-	private String when = "";
-	private String who = "";
-	private boolean enabled = true;
+    private static List<TriggerEvent> hooks = new ArrayList<TriggerEvent>();
+    private Map<EventCondition, String> eventConditions = new HashMap<EventCondition, String>();
+    private static String actionName = "";
+    /* Plugin information */
+    private String name = "";
+    private String url = "";
+    private String description = "";
+    private String shortName = "";
+    private String when = "";
+    private String who = "";
+    private boolean enabled = true;
 
-	private Map<Player, EventPlaceholderContainer> placeholders = new HashMap<Player, EventPlaceholderContainer>();
+    private Map<Player, EventPlaceholderContainer> placeholders = new HashMap<Player, EventPlaceholderContainer>();
 
-	private YamlBuilder configTemplate = null;
-	private int configVersion = 1;
-	private Plugin plugin = null;
+    private YamlBuilder configTemplate = null;
+    private int configVersion = 1;
+    private Plugin plugin = null;
 
-	private YamlStorage storage = null;
+    private YamlStorage storage = null;
 
-	public BaseEventHook(Plugin plugin, String shortName, int version) {
-		setPlugin(plugin);
-		setShortName(shortName);
-		this.configVersion = version;
-	}
+    public BaseEventHook(Plugin plugin, String shortName, int version) {
+        setPlugin(plugin);
+        setShortName(shortName);
+        this.configVersion = version;
+    }
 
-	public int getConfigVersion(){
-		return configVersion;
-	}
+    public int getConfigVersion() {
+        return configVersion;
+    }
 
-	public Map<EventCondition, String> getEventConditions() {
-		return this.eventConditions;
-	}
+    public Map<EventCondition, String> getEventConditions() {
+        return this.eventConditions;
+    }
 
-	public void loadConfig() {
-		if (storage.getConfig().getBoolean("enabled")) {
-			setEnabled(true);
-		} else {
-			setEnabled(false);
-		}
-	}
+    public void loadConfig() {
+        if (storage.getConfig().getBoolean("enabled")) {
+            setEnabled(true);
+        } else {
+            setEnabled(false);
+        }
+    }
 
-	public abstract void start();
+    public abstract void start();
 
-	public void generateConfig() {
-		if (getConfigTemplate() == null) {
-			YamlBuilder builder = new YamlBuilder();
-			builder.addPart("----------------------------------------");
-			builder.addPart(" " + getName());
-			builder.addPart(" " + getDescription());
-			builder.addPart(" " + getUrl());
-			builder.addPart("----------------------------------------");
-			builder.addPart(new YamlEmptyPart());
-			builder.addPart(" DO NOT EDIT THIS CONFIG VERSION!");
-			builder.addPart("config", configVersion);
-			builder.addPart(new YamlEmptyPart());
-			builder.addPart(new YamlEmptyPart());
-			builder.addPart(" Do you want to enable this event hook?");
-			builder.addPart(" Enabling this hook will trigger " + getActionName());
-			builder.addPart(" when " + getWhen());
-			builder.addPart(" for " + getWho());
-			builder.addPart("enabled", false);
-			builder.addPart(new YamlEmptyPart());
-			builder.addPart(" " + getActionName() + " to trigger");
-			builder.addPart(getActionName().toLowerCase(), "default");
-			builder.addPart(new YamlEmptyPart());
-			builder.addPart(new YamlEmptyPart());
-			setConfigTemplate(builder);
-		}
-	}
+    public void generateConfig() {
+        if (getConfigTemplate() == null) {
+            YamlBuilder builder = new YamlBuilder();
+            builder.addPart("----------------------------------------");
+            builder.addPart(" " + getName());
+            builder.addPart(" " + getDescription());
+            builder.addPart(" " + getUrl());
+            builder.addPart("----------------------------------------");
+            builder.addPart(new YamlEmptyPart());
+            builder.addPart(" DO NOT EDIT THIS CONFIG VERSION!");
+            builder.addPart("config", configVersion);
+            builder.addPart(new YamlEmptyPart());
+            builder.addPart(new YamlEmptyPart());
+            builder.addPart(" Do you want to enable this event hook?");
+            builder.addPart(" Enabling this hook will trigger " + getActionName());
+            builder.addPart(" when " + getWhen());
+            builder.addPart(" for " + getWho());
+            builder.addPart("enabled", false);
+            builder.addPart(new YamlEmptyPart());
+            builder.addPart(" " + getActionName() + " to trigger");
+            builder.addPart(getActionName().toLowerCase(), "default");
+            builder.addPart(new YamlEmptyPart());
+            builder.addPart(new YamlEmptyPart());
+            setConfigTemplate(builder);
+        }
+    }
 
-	/**
-	 * Get config
-	 * 
-	 * @return Config
-	 */
-	public YamlConfiguration getConfig() {
-		return storage.getConfig();
-	}
+    public void enableEvent(Player player) {
+        for (TriggerEvent event : getHooks())
+            event.enableEvent(player, getConfig().getString(BaseEventHook.getActionName().toLowerCase()));
+    }
 
-	/**
-	 * Get event hook name
-	 * 
-	 * @return Hook name
-	 */
-	public String getName() {
-		return name;
-	}
+    public void disableEvent(Player player) {
+        for (TriggerEvent event : getHooks())
+            event.disableEvent(player, getConfig().getString(BaseEventHook.getActionName().toLowerCase()));
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public static void registerTriggerEvent(TriggerEvent event) {
+        hooks.add(event);
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public static List<TriggerEvent> getHooks() {
+        return hooks;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setHooks(List<TriggerEvent> hooks) {
+        this.hooks = hooks;
+    }
 
-	public String getDescription() {
-		return description;
-	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    /**
+     * Get config
+     *
+     * @return Config
+     */
+    public YamlConfiguration getConfig() {
+        return storage.getConfig();
+    }
 
-	public String getShortName() {
-		return shortName;
-	}
+    /**
+     * Get event hook name
+     *
+     * @return Hook name
+     */
+    public String getName() {
+        return name;
+    }
 
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public YamlStorage getStorage() {
-		return storage;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setStorage(YamlStorage storage) {
-		this.storage = storage;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public String getWhen() {
-		return when;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setWhen(String action) {
-		this.when = action;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getWho() {
-		return who;
-	}
+    public String getShortName() {
+        return shortName;
+    }
 
-	public void setWho(String playersAction) {
-		this.who = playersAction;
-	}
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 
-	public static String getActionName() {
-		return actionName;
-	}
+    public YamlStorage getStorage() {
+        return storage;
+    }
 
-	public static void setActionName(String actionName) {
-		BaseEventHook.actionName = actionName;
-	}
+    public void setStorage(YamlStorage storage) {
+        this.storage = storage;
+    }
 
-	public YamlBuilder getConfigTemplate() {
-		return configTemplate;
-	}
+    public String getWhen() {
+        return when;
+    }
 
-	public void setConfigTemplate(YamlBuilder configTemplate) {
-		this.configTemplate = configTemplate;
-	}
+    public void setWhen(String action) {
+        this.when = action;
+    }
 
-	public Plugin getPlugin() {
-		return plugin;
-	}
+    public String getWho() {
+        return who;
+    }
 
-	public void setPlugin(Plugin plugin) {
-		this.plugin = plugin;
-	}
+    public void setWho(String playersAction) {
+        this.who = playersAction;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public static String getActionName() {
+        return actionName;
+    }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+    public static void setActionName(String actionName) {
+        BaseEventHook.actionName = actionName;
+    }
 
-	/**
-	 * Add a placeholder condition
-	 * 
-	 * @param condition
-	 *            Condition
-	 * @param value
-	 *            Value
-	 */
-	public BaseEventHook addCondition(EventCondition condition, String value) {
-		eventConditions.put(condition, value);
-		return this;
-	}
+    public YamlBuilder getConfigTemplate() {
+        return configTemplate;
+    }
 
-	public Map<Player, EventPlaceholderContainer> getPlaceholders() {
-		return placeholders;
-	}
+    public void setConfigTemplate(YamlBuilder configTemplate) {
+        this.configTemplate = configTemplate;
+    }
 
-	public void setPlaceholders(Map<Player, EventPlaceholderContainer> placeholders) {
-		this.placeholders = placeholders;
-	}
+    public Plugin getPlugin() {
+        return plugin;
+    }
 
-	public static enum EventCondition {
-		PLUGIN, VERSION, AUTHOR, OFFLINEPLAYER, PLAYER, MAIN, VERSION_IS_LOWER, VERSION_IS_HIGHER
-	}
+    public void setPlugin(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
-	public class EventPlaceholderContainer {
-		private Map<String, Object> placeholders = new HashMap<String, Object>();
+    public boolean isEnabled() {
+        return enabled;
+    }
 
-		public Map<String, Object> getPlaceholders() {
-			return placeholders;
-		}
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 
-		public void setPlaceholders(Map<String, Object> placeholders) {
-			this.placeholders = placeholders;
-		}
+    /**
+     * Add a placeholder condition
+     *
+     * @param condition Condition
+     * @param value     Value
+     */
+    public BaseEventHook addCondition(EventCondition condition, String value) {
+        eventConditions.put(condition, value);
+        return this;
+    }
 
-		public EventPlaceholderContainer addStaticPlaceholder(String key, Object value) {
-			placeholders.put(key, value);
-			return this;
-		}
-	}
+    public Map<Player, EventPlaceholderContainer> getPlaceholders() {
+        return placeholders;
+    }
+
+    public void setPlaceholders(Map<Player, EventPlaceholderContainer> placeholders) {
+        this.placeholders = placeholders;
+    }
+
+    public static enum EventCondition {
+        PLUGIN, VERSION, AUTHOR, OFFLINEPLAYER, PLAYER, MAIN, VERSION_IS_LOWER, VERSION_IS_HIGHER
+    }
+
+    public class EventPlaceholderContainer {
+        private Map<String, Object> placeholders = new HashMap<String, Object>();
+
+        public Map<String, Object> getPlaceholders() {
+            return placeholders;
+        }
+
+        public void setPlaceholders(Map<String, Object> placeholders) {
+            this.placeholders = placeholders;
+        }
+
+        public EventPlaceholderContainer addStaticPlaceholder(String key, Object value) {
+            placeholders.put(key, value);
+            return this;
+        }
+    }
 }
